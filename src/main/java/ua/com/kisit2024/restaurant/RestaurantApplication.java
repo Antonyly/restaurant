48e2929c5e0353df50250fada5b2f0b9020613be
Antonyly <antonlyzun@gmail.com>
2024-02-05 13:07:41 +0200

    +

---
+++ src/main/java/ua/com/kisit2024/restaurant/RestaurantApplication.java
@@ -0,0 +1,13 @@
+package ua.com.kisit2024.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
